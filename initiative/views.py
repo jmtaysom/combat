bcd1a615b0ad2eabea06a9ed2c583ade5583481a
jmtaysom <james.taysom@gmail.com>
2017-05-03 19:27:03 -0400

    started template for initiative tracker

--- initiative/views.py
+++ initiative/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Character
+
 # Create your views here.
 def index(request):
-    return HttpResponse('This will be the initiative tracker')
+    character_list = Character.objects.order_by('-initiative')
+    context = {'character_list': character_list}
+    return render(request, 'initiative/index.html', context)
 
 
 def hero(request, hero_name):
