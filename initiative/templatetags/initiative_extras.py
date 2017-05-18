f662f55c8110a4cdd07cf3df5dcbc7477868032e
jmtaysom <james.taysom@gmail.com>
2017-05-18 16:54:47 -0400

    removed f strings for deployment to 3.5 server

--- initiative/templatetags/initiative_extras.py
+++ initiative/templatetags/initiative_extras.py
@@ -6,6 +6,6 @@ register = template.Library()
 @register.filter
 def sign(value):
     if value > 0:
-        return f'+{value}'
+        return '+{}'.format(value)
     else:
-        return f'{value}'
+        return '{}'.format(value)
