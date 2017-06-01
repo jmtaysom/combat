0e392cfb5ab7079f2ddf007e1a2582a47b0bd135
jmtaysom <james.taysom@gmail.com>
2017-05-31 21:20:01 -0400

    first several URL paths

--- initiative/urls.py
+++ initiative/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /characters/
     url(r'^characters/$', views.characters, name='characters'),
     # ex: /Marius/
-    url(r'^(?P<hero_name>[\w\s]+)/$', views.hero, name='hero'),
-    url(r'^(?P<hero_name>[\w\s]+)/update/$', views.update, name='update'),
+    url(r'^characters/(?P<hero_name>[\w\s]+)/$', views.hero, name='hero'),
+    url(r'^characters/(?P<hero_name>[\w\s]+)/update/$', views.update, name='update'),
 
 ]
