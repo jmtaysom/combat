89c9729774673bfd9cf0d3617d313904d1cc1f07
jmtaysom <james.taysom@gmail.com>
2017-06-17 17:55:31 -0400

    removed completed todo

--- initiative/views.py
+++ initiative/views.py
@@ -12,7 +12,6 @@ from .models import Player, Monster
 
 # Create your views here.
 def index(request):
-    #TODO: need to add in monsters to the return.
     character_list = Player.objects.filter(present=True).order_by('-initiative')
     monster_list = Monster.objects.filter(count__gt=0)
     multiple_monsters = []
@@ -46,7 +45,6 @@ def monsters(request):
     CharacterFormSet = modelformset_factory(Monster, extra=0, form=CountForm)
     if request.method == 'POST':
         formset = CharacterFormSet(request.POST, request.FILES)
-        print(request.POST)
     else:
         formset = CharacterFormSet(queryset=Monster.objects.all())
     if formset.is_valid():
