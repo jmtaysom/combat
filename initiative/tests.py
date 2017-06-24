78769e920b476600d10ff90567b5e6e0deef452b
jmtaysom <james.taysom@gmail.com>
2017-06-23 22:45:51 -0400

    tested html response of characters

--- initiative/tests.py
+++ initiative/tests.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from initiative.views import index
+from initiative.views import index, characters
 
 
 class IndexTest(TestCase):
@@ -14,6 +14,21 @@ class IndexTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('\n\n<html>'))
+        self.assertIn('<title>Combat Tracker</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+
+class CharactersTest(TestCase):
+
+    def test_root_url_resolves_to_chaaracter_view(self):
+        found = resolve('/characters/')
+        self.assertEqual(found.func, characters)
+
+    def test_characters_returns_correct_html(self):
+        request = HttpRequest()
+        response = characters(request)
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('\n\n<html>'))
         self.assertIn('<title>Combat Tracker</title>', html)
